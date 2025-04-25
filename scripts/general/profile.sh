50426c19c3e3ea12dfc31ee7e59fdf9b0b8c8d9c
etrademark <etrademark.github@gmail.com>
2025-04-26 00:20:25 +0200

    Add further asus laptop support, to do; make it only availible for them.

---
+++ scripts/general/profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+asusctl profile --next
+profile=$(asusctl profile --profile-get | grep -Po 'Active profile is \K[^ ]+' | tr '[:upper:]' "[:lower:]")
+
+if [ $profile == "quiet" ]; then
+  profileNumber=33
+elif [ $profile == "balanced" ]; then
+  profileNumber=67
+elif [ $profile == "performance" ]; then
+  profileNumber=100
+else profileNumber=0; fi
+notify-send -h string:x-canonical-private-synchronous:volume_notif "Changed profile to ${profile}" -e -h int:value:$profileNumber -u critical
